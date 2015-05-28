c18ddc3318dd9d6e8db688f9d4cb1196ce28e894
shang101 <hang.shirley@gmail.com>
2015-05-27 23:36:49 -0400

    Committed by Udacity

---
+++ MemeMeApp/MemeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MemeViewController.swift
+//  MemeMeApp
+//
+//  Created by Shirley Hang on 5/17/15.
+//  Copyright (c) 2015 att. All rights reserved.
+//
+
+import UIKit
+
+class MemeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ }
